88b84e41755782d5c0c5255f442037c662f44a46
yuko.okada <yuko120129@gmail.com>
2020-12-14 10:58:08 +0900

    Add NewServlet.java

---
+++ src/controllers/NewServlet.java
@@ -0,0 +1,57 @@
+package controllers;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Task;
+import utils.DBUtill;
+
+/**
+ * Servlet implementation class NewServlet
+ */
+@WebServlet("/new")
+public class NewServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public NewServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em=DBUtill.createEntityManager();
+        em.getTransaction().begin();
+
+        Task t=new Task();
+
+
+        String content = "todo";
+        t.setContent(content);
+
+        Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+        t.setCreated_at(currentTime);
+        t.setUpdated_at(currentTime);
+
+
+        em.persist(t);
+        em.getTransaction().commit();
+
+        response.getWriter().append(Integer.valueOf(t.getId()).toString());
+
+        em.close();
+    }
+
+}
